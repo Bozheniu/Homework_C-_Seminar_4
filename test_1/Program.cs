58a2407c150906a7d7c1cadafb35cb0756e8082a
Bozheniu <bozheniu@gmail.com>
2022-11-05 15:16:30 +0100

    Добавила решение к задаче 25

---
+++ test_1/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 25: Напишите цикл, 
+который принимает на вход два числа (A и B) и возводит число A
+в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4-> 16 */
+
+double RaisingAtoThePowerOfB(double A, double B)
+{
+    double i = Math.Pow(A, B);
+    return i;
+}
+
+Console.WriteLine(RaisingAtoThePowerOfB(3, 5));
+Console.WriteLine(RaisingAtoThePowerOfB(2, 4));
